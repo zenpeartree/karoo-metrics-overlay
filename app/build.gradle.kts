2ea49a093e814fd70909942d87d65560aa372f6f
Joao Miguel Alves Pereira <joao.pereira@zendesk.com>
2026-03-16 11:48:27 +0000

    Bump version to 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.zenpeartree.karoometricsoverlay"
         minSdk = 23
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.2.1"
     }
 
     signingConfigs {
